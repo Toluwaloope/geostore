ee9c1ce5f8ebf50bbd568a9d73d36111a581c33d
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-22 03:24:43 +0100

    Update create_database.sh

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -2,10 +2,10 @@
 
 psql createdb -U postgres geostore
 
-psql -U geostore -d geostore -f /usr/share/tomcat/webapps/001_setup_db.sql
+psql -U geostore -d geostore -f 001_setup_db.sql
 
-psql -U geostore -d geostore -f /usr/share/tomcat/webapps/002_create_schema_postgres.sql
+psql -U geostore -d geostore -f 002_create_schema_postgres.sql
 
 set PGOPTIONS="--search_path=geostore_test"
 
-psql -U geostore_test -d geostore -f /usr/share/tomcat/webapps/002_create_schema_postgres.sql
+psql -U geostore_test -d geostore -f 002_create_schema_postgres.sql
