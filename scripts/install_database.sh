33d0ee21114d4b062966c0f91a3db4580cf38d6c
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-22 13:10:32 +0100

    Update install_database.sh

--- scripts/install_database.sh
+++ scripts/install_database.sh
@@ -6,10 +6,10 @@ cd $TOMCATWEBDIR/geostore/
 
 ./$DEPLOYDIR/run4MS.sh
 
-#sudo yum update -y
+sudo yum update -y
 
-#sudo yum install -y postgresql postgresql-contrib
+sudo yum install -y postgresql postgresql-contrib
 
-#sudo systemctl start postgresql
+sudo systemctl start postgresql
 
-#./usr/share/tomcat/webapps/create_database.sh
+./usr/share/tomcat/webapps/create_database.sh
