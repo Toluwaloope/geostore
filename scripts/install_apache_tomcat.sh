41a411d367a149b0a4fabaa2058a93faad2d73cf
Toluwaloope <71833008+Toluwaloope@users.noreply.github.com>
2022-06-24 03:56:09 +0100

    Update install_apache_tomcat.sh

--- scripts/install_apache_tomcat.sh
+++ scripts/install_apache_tomcat.sh
@@ -3,14 +3,14 @@
 sudo apt-get update
 sudo apt install -y default–jdk
 sudo groupadd tomcat
-sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+sudo useradd -s /bin/false -g tomcat -d /usr/share/tomcat tomcat
 cd /tmp
 wget http://apache.cbox.biz/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz
 wget https://www.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz.sha512
-sudo mkdir /opt/tomcat
-sudo tar xzvf apache-tomcat-9.0.14.tar.gz -C /opt/tomcat --strip-components=1
+sudo mkdir /usr/share/tomcat
+sudo tar xzvf apache-tomcat-9.0.14.tar.gz -C /usr/share/tomcat --strip-components=1
 cd /opt/tomcat
-sudo chgrp -R tomcat /opt/tomcat
+sudo chgrp -R tomcat /usr/share/tomcat
 sudo chmod -R g+r conf
 sudo chown -R tomcat webapps/ work/ temp/ logs/
 sudo chmod g+x conf
